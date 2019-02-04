3912263213b16bdf0d500194200feb20e0f7647d
ouss <bnssuo@mail.com>
2019-02-04 11:28:10 +0100

    Updates

---
+++ precache-manifest.105da9b8c59af15af5f9baddf51cee77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "325204454d0e026e4ddb",
+    "url": "/reactTest/static/js/runtime~main.32520445.js"
+  },
+  {
+    "revision": "a7312c5667c2a5fea045",
+    "url": "/reactTest/static/js/main.a7312c56.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/reactTest/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "a7312c5667c2a5fea045",
+    "url": "/reactTest/static/css/main.9713bf40.chunk.css"
+  },
+  {
+    "revision": "2837fa1d489441b0f1185296ad85341f",
+    "url": "/reactTest/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactTest/precache-manifest.65e9bbb99cba82e5c9078b1d024ab798.js"
+  "/reactTest/precache-manifest.105da9b8c59af15af5f9baddf51cee77.js"
 );
 
 workbox.clientsClaim();
